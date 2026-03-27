0ac047a2e9c8916cce7146f9c92bf3e4e28110b4
Sanae-123-wq <sanaeelbelghiti2@gmail.com>
2026-03-27 15:34:10 +0100

    create hero section- create favories section- update footer- update cards

--- server/models/Product.js
+++ server/models/Product.js
@@ -2,12 +2,20 @@ import mongoose from 'mongoose';
 
 const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
+  name_fr: { type: String },
+  name_ar: { type: String },
   description: { type: String },
+  description_fr: { type: String },
+  description_ar: { type: String },
   price: { type: Number, required: true },
   image: { type: String },
   category: { type: String, required: true },
   stock: { type: Number, default: 0 },
   isAvailable: { type: Boolean, default: true },
+  vendorId: { type: String },
+
+  approvalStatus: { type: String, enum: ['pending', 'approved', 'rejected'], default: 'pending' },
+
 }, { timestamps: true });
 
 const Product = mongoose.model('Product', productSchema);
