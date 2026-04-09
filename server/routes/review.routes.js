9cc832c08196a66f86ac8e120fb87ec4c303e375
Sanae-123-wq <sanaeelbelghiti2@gmail.com>
2026-04-09 17:59:32 +0100

    update categories profiles, add reclamations, add reviews, add zoom

--- server/routes/review.routes.js
+++ server/routes/review.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { createReview, getPublicReviews } from '../controllers/review.controller.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.post('/', createReview);
+router.post('/', protect, createReview);
 router.get('/', getPublicReviews);
 
 export default router;
